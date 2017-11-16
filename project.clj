ceccdcc6fc475d794162cf8b1b8e980ab76e467e
Naylyn Gaffney <naylyn.gaffney@gmail.com>
2017-11-16 21:24:10 +1100

    Update description in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject exp-quil "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Naylyn's quil sandbox project."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
