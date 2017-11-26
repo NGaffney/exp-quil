9c4610251142ab28c941821678d9f0fb9edfb060
Naylyn Gaffney <naylyn.gaffney@gmail.com>
2017-11-26 16:56:41 +1100

    Cleanup a bit.

--- src/exp_quil/square_tiles.clj
+++ src/exp_quil/square_tiles.clj
@@ -28,10 +28,7 @@
 
 (defn setup []
   ; (q/frame-rate 60)
-  (q/color-mode :rgb)
-  (q/no-stroke)
-  (q/background 255)
-  (q/fill 0))
+  (q/background 255))
 
 (defn do-matrix-indexed [f m]
   (do (mat/emap-indexed f m)))
@@ -53,38 +50,12 @@
           (map elem-fn args)))
       x y)))
 
-(defn draw-fn1 []
-  (do
-    (q/background 0)
-    (do-matrix-indexed
-      (fn [idx i]
-        (let [[x y] idx]
-          (q/set-pixel x y (* 255 (- 1 i)))
-          (q/update-pixels)))
-      tst3)))
+(defn greyerise
+  "Transform a value in the range 0 1 to an RGB grayscale image."
+  [x]
+  (q/color (* 255 (- 1 x))))
 
-(defn draw-fn2 []
-  (do-matrix-indexed
-    (fn [idx i]
-      (let [[x y] idx]
-        (if-not (zero? i)
-          (q/point x y))))
-    tst3))
-
-(defn draw-fn3 []
-  (do-matrix-indexed
-    (fn [idx i]
-      (let [[x y] idx]
-        (q/fill (* 255 (- 1 i)))
-        (q/rect x y 1 1)))
-    tst3))
-
-(defn draw-fn4 []
-  (do
-    (q/pixels (mat/to-vector tst3))
-    (q/update-pixels)))
-
-(defn draw-fn5 []
+(defn draw-fn []
   (let [img (q/create-image
               (first grid-size)
               (second grid-size)
@@ -92,23 +63,11 @@
     (do-matrix-indexed
       (fn [idx i]
         (let [[x y] idx]
-          (q/set-pixel img x y 0)))
-      tst3)
-    (q/image img 0 0)))
-
-(defn draw-fn6 []
-  (let [img (q/create-image
-              (first grid-size)
-              (second grid-size)
-              :rgb)]
-    (do-matrix-indexed
-      (fn [idx i]
-        (let [[x y] idx]
-          (q/set-pixel img x y (apply q/color (repeat 3 (* 255 (- 1 i)))))))
+          (q/set-pixel img x y (greyerise i))))
       tst3)
     (q/image img 0 0)))
 
 (q/defsketch square-tiles
   :setup setup
   :size grid-size
-  :draw draw-fn6)
+  :draw draw-fn)
